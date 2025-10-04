4ffd427c83b2feb6864d496d613172ffc586bbe0
Aman412000 <160015399+Aman412000@users.noreply.github.com>
2025-10-04 15:46:29 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -5,4 +5,8 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID amazom machine image :: 
     instance_type = "t2.micro"
+    subnet_id     =      # wirte ur subnet id from cloud ec2 machine 
+    key_name      =    # on aws search for key pair paste here : like ("aws_login")
+
 }
+
